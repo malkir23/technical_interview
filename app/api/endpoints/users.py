429e97de511a9aae91b92f23185b901893be365c
malkir <mandragorand@gmail.com>
2025-04-24 12:41:52 +0200

    refactor user endpoint functions: update session dependency type for clarity

--- app/api/endpoints/users.py
+++ app/api/endpoints/users.py
@@ -3,16 +3,19 @@ from app.repositories.user_repository import UserRepository
 from app.models.users import UserSchema, UserCreate, UserUpdate
 from fastapi import Depends
 from app.database.database import get_session
+from sqlalchemy.orm import Session
 
 
 router = APIRouter()
 
 @router.get("/users", response_model=list[UserSchema])
-def list_users(session: Depends(get_session)) -> list[UserSchema]:
+def list_users(session: Session = Depends(get_session)) -> list[UserSchema]:
     users = UserRepository(session).get_all_users() or []
     return users
 
 @router.post("/users", response_model=UserCreate)
-def create_user(user: UserCreate, session: Depends(get_session)) -> UserCreate:
+def create_user(
+    user: UserCreate, session: Session = Depends(get_session)
+) -> UserCreate:
     user = UserRepository(session).create_user(**user.dict())
     return user
